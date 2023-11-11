d2e1bf9d077867f8cad54b13fe75e6b58fc998a7
Kenzo Wijnants <kenzo.wijnants@appwise.be>
2023-11-11 04:34:42 +0100

    WIP Embla slider.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,35 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import emblaCarouselSvelte from 'embla-carousel-svelte'
+	import Autoplay from 'embla-carousel-autoplay'
+	import pc from '$lib/images/pc.jpg'
+	import phone from '$lib/images/phone.jpg'
+	import coding from '$lib/images/coding.jpg'
+
+	let emblaApi
+	let options = { loop: true }
+	let plugins = [Autoplay()]
+
+	const onInit = (event) => {
+		emblaApi = event.detail
+		// console.log(emblaApi.slideNodes()) // Access API
+	}
+</script>
+
+<div
+	class="embla overflow-hidden"
+	use:emblaCarouselSvelte={{ options, plugins }}
+	on:emblaInit={onInit}
+>
+	<div class="embla__container flex">
+		<div class="embla__slide"><img src={pc} alt="PC repair header" /></div>
+		<div class="embla__slide"><img src={phone} alt="Phone repair header" /></div>
+		<div class="embla__slide"><img src={coding} alt="Coding and webdesigm header" /></div>
+	</div>
+</div>
+
+<style lang="postcss">
+	.embla__slide {
+		flex: 0 0 30%;
+		min-width: 0;
+	}
+</style>
