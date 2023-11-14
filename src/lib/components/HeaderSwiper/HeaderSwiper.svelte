6ae970d26df66d92e7a32cf94a60c42459f5d9c2
Kenzo Wijnants <kenzo.wijnants@appwise.be>
2023-11-14 23:04:16 +0100

    Added text to swiper component + styling.

--- src/lib/components/HeaderSwiper/HeaderSwiper.svelte
+++ src/lib/components/HeaderSwiper/HeaderSwiper.svelte
@@ -2,7 +2,7 @@
 	import { onMount } from 'svelte'
 	import { register } from 'swiper/element/bundle'
 
-	export let images: string[] = []
+	export let images: { src: string; text: string }[] = []
 
 	onMount(() => {
 		register()
@@ -21,9 +21,18 @@
 	autoplay-disable-on-interaction="true"
 	class="h-[50vh] max-w-[100vw]"
 >
-	{#each images as imgSrc}
+	{#each images as { src: imgSrc, text: imgText }}
 		<swiper-slide class="w-full">
 			<img src={imgSrc} alt="" class="h-[920px] w-full object-cover" />
+			<h1
+				class="absolute left-[50%] top-[40%] -translate-x-1/2 -translate-y-1/2 transform text-center text-7xl text-white"
+			>
+				{imgText}
+			</h1>
+			<button
+				class="absolute left-[50%] top-[60%] inline-block -translate-x-1/2 -translate-y-1/2 transform rounded-xl border-none bg-gray-800 bg-opacity-60 p-[1rem] text-center text-4xl text-white no-underline"
+				>Klik hier</button
+			>
 		</swiper-slide>
 	{/each}
 	<div class="swiper-pagination justify-center text-center"></div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,12 @@
 	import pc from '$lib/images/pc.jpg'
 	import phone from '$lib/images/phone.jpg'
 	import coding from '$lib/images/coding.jpg'
-	let images = [pc, phone, coding]
+
+	let images = [
+		{ src: pc, text: 'Herstelling nodig?' },
+		{ src: phone, text: 'Smartphone reparaties' },
+		{ src: coding, text: 'Webdesign' }
+	]
 </script>
 
 <div class="h-[124rem] overflow-hidden">
