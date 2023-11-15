ecafd3c341f44a4f7a4e3c244947bd5685d65da6
Kenzo <kenzo.wijnants@appwise.be>
2023-11-15 20:10:26 +0100

    Added colors and media query sizes to Tailwind.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -17,9 +17,26 @@ const config = {
 			'5xl': '3rem',
 			'6xl': '4rem',
 			'7xl': '5rem'
+		},
+		colors: {
+			black: '#000000ff',
+			white: '#ffffffff',
+			'orange-web': '#ffa400ff',
+			'royal-blue-traditional': '#2a2a72ff',
+			'raisin-black': '#232528ff',
+			'alice-blue': '#eaf6ffff',
+			azure: '#3a86ffff'
+		},
+		screens: {
+			'2xl': { max: '1535px' },
+			xl: { max: '1279px' },
+			lg: { max: '1023px' },
+			md: { max: '850px' },
+			sm: { max: '650px' },
+			xs: { max: '450px' },
+			'2xs': { max: '400px' }
 		}
 	},
-
 	plugins: []
 }
 
