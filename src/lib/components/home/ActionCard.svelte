f5f28ee2c456961def4e79199c62dc98d29e1475
Kenzo Wijnants <kenzo.wijnants@appwise.be>
2023-11-17 03:05:39 +0100

    Added Card component + sample data.

---
+++ src/lib/components/home/ActionCard.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import type { ActionCard } from '$lib/types/components/actioncard'
+	export let cardData: ActionCard
+</script>
+
+<div
+	class="border-subtle-gray flex h-full w-full flex-col items-center justify-between border border-solid bg-transparent px-[15px] py-[40px]"
+>
+	<div class="h-[80px] w-[80px]">
+		<img src={cardData.image.url} alt={cardData.image.alt} />
+	</div>
+	<h2 class="text-4xl font-suisse">{cardData.title}</h2>
+	<p class="text-2xl text-center mb-[1rem]">{cardData.description}</p>
+	<button
+		class="bg-gradient-top-right flex h-[40px] w-[120px] cursor-pointer items-center justify-center rounded-xl border border-solid border-alice-blue px-[1rem] py-[0.6rem] text-center"
+	>
+		<a
+			class="leading-[1.2 rem] text-[2rem] font-bold text-alice-blue"
+			href={cardData.button.url}
+			target={cardData.button.target}>{cardData.button.text}</a
+		>
+	</button>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,23 +3,72 @@
 	import pc from '$lib/assets/images/pc.jpg'
 	import phone from '$lib/assets/images/phone.jpg'
 	import coding from '$lib/assets/images/coding.jpg'
+	import pcIcon from '$lib/assets/icons/desktop.svg'
+	import phoneIcon from '$lib/assets/icons/phone.svg'
+	import webIcon from '$lib/assets/icons/website.svg'
+	import ActionCard from '$lib/components/home/ActionCard.svelte'
 
 	let images = [
 		{ src: pc, text: 'Herstelling nodig?' },
 		{ src: phone, text: 'Smartphone reparaties' },
 		{ src: coding, text: 'Webdesign' }
 	]
+
+	let data1 = {
+		image: {
+			url: pcIcon,
+			alt: 'Computer herstellingen'
+		},
+		title: 'Computer herstellingen',
+		description: 'Werkt uw computer niet meer zoals het hoort? Wij helpen u graag verder!',
+		button: {
+			url: '#',
+			text: 'Lees meer',
+			target: '_self' as '_self'
+		}
+	}
+
+	let data2 = {
+		image: {
+			url: phoneIcon,
+			alt: 'Smartphone herstellingen'
+		},
+		title: 'Smartphone herstellingen',
+		description: 'Problemen met uw smartphone? Gebarsten scherm? Wij herstellen het voor u!',
+		button: {
+			url: 'https://google.com',
+			text: 'Lees meer',
+			target: '_self' as '_self'
+		}
+	}
+
+	let data3 = {
+		image: {
+			url: webIcon,
+			alt: 'Webdesign'
+		},
+		title: 'Webdesign',
+		description:
+			'Wij maken uw website op maat! Van eenvoudige websites tot uitgebreide web applicaties.',
+		button: {
+			url: 'https://google.com',
+			text: 'Lees meer',
+			target: '_self' as '_self'
+		}
+	}
 </script>
 
 <!--Swiper-->
-<header class="mb-[2rem] overflow-hidden">
+<header class="mb-[4rem] overflow-hidden">
 	<HeaderSwiper {images} />
 </header>
 <!--Cards-->
 <section>
-	<div class="mx-auto mb-[2rem] grid h-[300px] w-[1440px] grid-cols-3 gap-[4rem] bg-black">
-		<div class="h-full w-full place-self-center bg-orange-web">test1</div>
-		<div class="h-full w-full place-self-center bg-azure">test2</div>
-		<div class="h-full w-full place-self-center bg-raisin-black">test3</div>
+	<div
+		class="mx-auto mb-[4rem] grid h-[300px] max-w-[1440px] grid-cols-3 gap-[4rem] bg-transparent"
+	>
+		<ActionCard cardData={data1} />
+		<ActionCard cardData={data2} />
+		<ActionCard cardData={data3} />
 	</div>
 </section>
