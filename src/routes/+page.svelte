1a9a15431b7bf0019a31e8c532aae56526db25e9
Kenzo Wijnants <kenzo.wijnants@appwise.be>
2023-11-17 01:11:38 +0100

    Added grid for action cards.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,6 +11,15 @@
 	]
 </script>
 
-<div class="h-[124rem] overflow-hidden">
+<!--Swiper-->
+<header class="mb-[2rem] overflow-hidden">
 	<HeaderSwiper {images} />
-</div>
+</header>
+<!--Cards-->
+<section>
+	<div class="mx-auto mb-[2rem] grid h-[300px] w-[1440px] grid-cols-3 gap-[4rem] bg-black">
+		<div class="h-full w-full place-self-center bg-orange-web">test1</div>
+		<div class="h-full w-full place-self-center bg-azure">test2</div>
+		<div class="h-full w-full place-self-center bg-raisin-black">test3</div>
+	</div>
+</section>
