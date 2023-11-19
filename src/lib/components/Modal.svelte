39d2e7f2f9091d70d9ee22413c607957af0f4a97
Kenzo Wijnants <kenzo.wijnants@appwise.be>
2023-11-20 00:15:20 +0100

    Added modal close functionality.

--- src/lib/components/Modal.svelte
+++ src/lib/components/Modal.svelte
@@ -1,12 +1,58 @@
-<script>
+<script lang="ts">
 	export let show = false
+
 	import { portal } from '$lib/portal'
+	import { onMount, createEventDispatcher } from 'svelte'
+
+	let keydownHandler: (event: { key: string }) => void
+	// Create event dispatcher
+	const dispatch = createEventDispatcher()
+
+	// Dispatch close to parent component and destroy keydown event listener
+	function closeModal() {
+		show = false
+
+		dispatch('close')
+	}
+
+	// Close modal on escape key press
+	onMount(() => {
+		keydownHandler = (event) => {
+			if (event.key === 'Escape') {
+				closeModal()
+			}
+		}
+		window.addEventListener('keydown', keydownHandler)
+	})
+	// Close modal on outside click
+	function outsideClick(event: any) {
+		closeModal()
+	}
+
+	// Prevent click event from propagating to parent elements
+	function insideClick(event: any) {
+		event.stopPropagation()
+	}
 </script>
 
 {#if show}
-	<div use:portal class="fixed inset-0 z-50 flex flex-col items-center justify-center bg-black/60">
-		<div class="mx-auto w-full max-w-[800px] rounded-2xl bg-white p-6">
+	<!-- svelte-ignore a11y-click-events-have-key-events -->
+	<!-- svelte-ignore a11y-no-static-element-interactions -->
+	<div
+		use:portal
+		class="fixed inset-0 z-50 flex flex-col items-center justify-center bg-black/60"
+		on:click={outsideClick}
+	>
+		<div class="mx-auto w-full max-w-[800px] rounded-2xl bg-white p-6" on:click={insideClick}>
 			<slot />
+			<button
+				on:click|stopPropagation={closeModal}
+				class="absolute right-10 top-0 text-[4rem] font-semibold text-azure hover:cursor-pointer hover:text-orange-web"
+				>&times;</button
+			>
 		</div>
 	</div>
 {/if}
+
+<style lang="postcss">
+</style>
