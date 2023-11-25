7f703408387972be77dbe67c35009ae3c5c35d96
Kenzo Wijnants <kenzo.wijnants@appwise.be>
2023-11-25 02:53:51 +0100

    Styled price items.

--- src/lib/components/hersteldienst/PriceItem.svelte
+++ src/lib/components/hersteldienst/PriceItem.svelte
@@ -6,18 +6,20 @@
 </script>
 
 <div
-	class="flex min-w-[400px] flex-col border-l-2 border-transparent px-[2rem] hover:border-l-orange-web"
+	class="flex h-[380px] min-w-[300px] flex-col items-center justify-center border border-subtle-gray py-[2rem]"
 >
-	<h2 class="mb-[1rem] font-suisse text-4xl font-medium">
+	<h2 class="font-suise mb-[1rem] text-4xl font-medium">
 		{data.title}
 	</h2>
-	<p class="font-suise mb-[3rem] text-2xl font-normal">
+	<p class="font-suise mb-[3rem] text-5xl font-semibold">
 		{data.price}
 	</p>
-	<ul>
+	<ul class="mb-[3rem] flex flex-col items-start">
 		{#each data.features as features (features)}
-			<li>{features}</li>
+			<li class="font-suise list-disc text-left text-2xl">{features}</li>
 		{/each}
 	</ul>
-	<LinkButton data={data.button}></LinkButton>
+	<div class="flex justify-center">
+		<LinkButton data={data.button}></LinkButton>
+	</div>
 </div>
