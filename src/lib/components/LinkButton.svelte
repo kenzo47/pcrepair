9ca35d8b22feb7f3e7ca665ec7b8de4b8874b9b3
Kenzo Wijnants <kenzo.wijnants@appwise.be>
2023-12-02 22:33:09 +0100

    Remov eborder from button.

--- src/lib/components/LinkButton.svelte
+++ src/lib/components/LinkButton.svelte
@@ -5,6 +5,6 @@
 </script>
 
 <button
-	class="flex h-[45px] transform cursor-pointer items-center justify-center rounded-2xl border border-solid border-alice-blue bg-gradient-top-right px-[1rem] py-[0.6rem] text-center href={data.url} target={data.target}"
+	class="bg-gradient-top-right flex h-[45px] transform cursor-pointer items-center justify-center rounded-2xl px-[1rem] py-[0.6rem] text-center href={data.url} target={data.target}"
 	><span class="leading-[1.2 rem] text-[2rem] font-bold text-white">{data.text}</span></button
 >
