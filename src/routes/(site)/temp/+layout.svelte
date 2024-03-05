247f3eef239b65aa8f8d5eeb12f1f64cc5e414c7
Kenzo Wijnants <kenzo.wijnants@appwise.be>
2024-03-05 17:49:29 +0100

    Update dependencies.

--- src/routes/(site)/temp/+layout.svelte
+++ src/routes/(site)/temp/+layout.svelte
@@ -2,10 +2,8 @@
 	import '$lib/assets/css/app.postcss'
 	import Footer from '$lib/components/Footer.svelte'
 	import Navigation from '$lib/components/Navigation.svelte'
-	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit'
 
 	// export let data: LayoutData
-	injectSpeedInsights()
 </script>
 
 <Navigation />
