731c6dbbdeae85f1e6e1630e59968607d6f40e6d
Kenzo <kenzo.wijnants@appwise.be>
2024-03-05 22:51:51 +0100

    WIP Hamburger menu for mobile.

--- src/lib/components/Navigation.svelte
+++ src/lib/components/Navigation.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
-	import logo from '$lib/assets/icons/logo.svg'
 	import { page } from '$app/stores'
+	import hamburger from '$lib/assets/icons/hamburger.svg'
+	import logo from '$lib/assets/icons/logo.svg'
+	import Modal from './Modal.svelte'
+
+	let showModal = false
 </script>
 
 <nav class="wrapper flex h-[100px] w-full items-center justify-between p-4">
@@ -9,7 +13,7 @@
 			<img src={logo} alt="Reboot-IT Logo" class="h-[60px] w-[240px]" />
 		</a>
 	</div>
-	<div class="menu flex gap-[8rem] text-center text-[2rem]">
+	<div class="menu flex gap-[8rem] text-center text-[2rem] md:hidden">
 		<a href="/" class:active={$page.url.pathname === '/'}>Home</a>
 		<a href="/hersteldienst" class:active={$page.url.pathname === '/hersteldienst'}>Hersteldienst</a
 		>
@@ -17,6 +21,20 @@
 		<a href="/over-ons" class:active={$page.url.pathname === '/over-ons'}>Over ons</a>
 		<a href="/contact" class:active={$page.url.pathname === '/contact'}>Contact</a>
 	</div>
+	<Modal show={showModal} on:close={() => (showModal = false)}>
+		<div class="flex flex-col gap-4 overflow-hidden text-center text-[5rem] text-white">
+			<a href="/" class:active={$page.url.pathname === '/'}>Home</a>
+			<a href="/hersteldienst" class:active={$page.url.pathname === '/hersteldienst'}
+				>Hersteldienst</a
+			>
+			<a href="/webdesign" class:active={$page.url.pathname === '/webdesign'}>Webdesign</a>
+			<a href="/over-ons" class:active={$page.url.pathname === '/over-ons'}>Over ons</a>
+			<a href="/contact" class:active={$page.url.pathname === '/contact'}>Contact</a>
+		</div>
+	</Modal>
+	<button class="hamburger hidden md:block" on:click={() => (showModal = true)}>
+		<img src={hamburger} alt="Hamburger Icon" class="h-[30px] w-[30px]" />
+	</button>
 </nav>
 
 <style lang="postcss">
@@ -26,5 +44,9 @@
 
 	.menu a:hover {
 		@apply scale-110 transform-gpu duration-300 ease-in-out;
+	}
+
+	.modal a {
+		@apply my-[4rem];
 	}
 </style>
