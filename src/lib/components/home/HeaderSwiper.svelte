85cca8167a7287049a34e6656f4cb9b60a2e9d1c
Kenzo <kenzo.wijnants@appwise.be>
2024-03-06 00:17:08 +0100

    Fix swiper on mobile.

--- src/lib/components/home/HeaderSwiper.svelte
+++ src/lib/components/home/HeaderSwiper.svelte
@@ -26,18 +26,18 @@
 				style="background-image: url({item.image.url})"
 			></div>
 			<h1
-				class="absolute left-[50%] top-[25%] -translate-x-1/2 transform text-center font-roboto text-[6rem] text-white shadow-black text-shadow"
+				class="absolute left-[50%] top-[25%] -translate-x-1/2 transform whitespace-nowrap text-center font-roboto text-[6rem] text-white shadow-black text-shadow"
 			>
 				{item.title}
 			</h1>
 			<p
-				class="absolute left-[50%] top-[45%] -translate-x-1/2 transform text-center font-roboto text-[2rem] font-medium text-white shadow-black text-shadow"
+				class="absolute left-[50%] top-[48%] -translate-x-1/2 transform text-center font-roboto text-[2rem] font-medium text-white shadow-black text-shadow md:w-[40rem]"
 			>
 				{item.description}
 			</p>
 			<button
 				on:click={() => goto(item.button.url ? item.button.url : '/')}
-				class="absolute left-[50%] top-[65%] flex h-[45px] w-[120px] -translate-x-1/2 -translate-y-1/2 transform cursor-pointer items-center justify-center rounded-2xl bg-gradient-top-right px-[1rem] py-[0.6rem] text-center"
+				class="absolute left-[50%] top-[72%] flex h-[45px] w-[120px] -translate-x-1/2 -translate-y-1/2 transform cursor-pointer items-center justify-center rounded-2xl bg-gradient-top-right px-[1rem] py-[0.6rem] text-center"
 				><span class="font-roboto text-[2.2rem] font-bold text-white">
 					{#if item.button.text}
 						{item.button.text}
