92651614b6ae2c58bdbe60886875ab7ec15b5fd5
Kenzo <kenzo.wijnants@appwise.be>
2024-03-18 23:40:47 +0100

    Move airtable API functionality to separate function.

---
+++ src/lib/airtable.ts
@@ -0,0 +1,30 @@
+import { AIRTABLE_API_KEY, AIRTABLE_BASE_ID } from '$env/static/private';
+
+
+
+export async function submitToAirtable(form): Promise<void> {
+  const { name, email, phone, message } = form.data;
+  const AIRTABLE_URL = `https://api.airtable.com/v0/${AIRTABLE_BASE_ID}/submissions`;
+
+  const data = {
+    records: [
+      {
+        fields: {
+          'Name': name,
+          'Email': email,
+          'Phone': phone,
+          'Message': message,
+        },
+      },
+    ],
+  };
+
+  await fetch(AIRTABLE_URL, {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${AIRTABLE_API_KEY}`,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  });
+}
