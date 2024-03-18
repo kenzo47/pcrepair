97d3923c76c736daa50a10ba645b9d199e31f140
Kenzo <kenzo.wijnants@appwise.be>
2024-03-18 23:21:31 +0100

    Optional phone regex.

--- src/lib/schemas/ContactForm.ts
+++ src/lib/schemas/ContactForm.ts
@@ -1,8 +1,11 @@
-import { z } from 'zod'
+import { z } from 'zod';
+
+// const phoneRegex = /^(\+\d{1,3}[- ]?)?\d{10}$/;
 
 export const schema = z.object({
 	name: z.string().min(2, { message: 'Voer een geldige naam in.' }),
 	email: z.string().email().min(1, { message: 'Voer een geldig email adres in.' }),
+	// phone: z.string().optional().refine(phone => phoneRegex.test(phone || ''), { message: 'Voer een geldig telefoonnummer in.' }),
 	phone: z.string().optional(),
 	message: z.string().min(10, { message: 'Voer een geldig bericht in.' })
 })
