1e5ca87db652fe50d7e9fa10d3e94bda2d0c90b4
Kenzo <kenzo.wijnants@appwise.be>
2024-03-24 23:51:12 +0100

    Add vercel analytics.

--- src/routes/(site)/+layout.ts
+++ src/routes/(site)/+layout.ts
@@ -1,11 +1,12 @@
+import { inject } from '@vercel/analytics';
 import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
 import type { LayoutLoad } from './$types';
 
+inject();
 injectSpeedInsights();
 
-export const load: LayoutLoad = async({ url }) => {
-
+export const load: LayoutLoad = async ({ url }) => {
 	return {
-		url: url.pathname
-	}
-}
+		url: url.pathname,
+	};
+};
